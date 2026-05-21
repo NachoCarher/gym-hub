79c5f56fa53c46bed8e0fcbd865fae9fa76c1192
nachocarher <nachoch64@correo.ugr.es>
2026-05-21 15:46:40 +0200

    Add Material-UI components and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import './App.css'
+import { Button } from '@mui/material'
 
 function App() {
   return (
     <>
       <h1>Gym Hub</h1>
+      <Button variant='contained'>Welcome! 🎉</Button>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ScopedCssBaseline>
+      <App />
+    </ScopedCssBaseline>
   </StrictMode>,
 )
