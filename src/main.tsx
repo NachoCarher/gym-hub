3e17e12b6c33c05bcba8c73d2737b1758392d45c
nachocarher <nachoch64@correo.ugr.es>
2026-05-24 01:36:10 +0200

    Add QueryProvider for React Query integration

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
+import { QueryProvider } from './app/providers/query-provider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ScopedCssBaseline>
-      <App />
+      <QueryProvider>
+        <App />
+      </QueryProvider>
     </ScopedCssBaseline>
   </StrictMode>,
 )
