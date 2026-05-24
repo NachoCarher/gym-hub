ad984892fd15d9465fe91992f697725cb852eab7
nachocarher <nachoch64@correo.ugr.es>
2026-05-24 02:00:26 +0200

    Add WorkoutsList component to display workout data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-
 import './App.css'
 import { Button } from '@mui/material'
+import { WorkoutsList } from './components/WorkoutsList'
+
 
 function App() {
   return (
     <>
       <h1>Gym Hub</h1>
       <Button variant='contained'>Welcome! 🎉</Button>
+      <WorkoutsList />
     </>
   )
 }
---
+++ src/components/WorkoutsList.tsx
@@ -0,0 +1,41 @@
+import { useWorkouts } from '../features/workouts/hooks/useWorkouts'
+
+export function WorkoutsList(){
+  const { isLoading, isError, data } = useWorkouts()
+
+  if (isLoading) return <p>Cargando...</p>
+  if (isError) return <p>Ha habido un error 🙅🏼...</p>
+
+  return <ul>
+    {
+      data?.map(workout => (
+        <li key={workout.id}>
+          {workout.date} Ejercicio {workout.id}
+
+          <ul>
+            {
+              workout.exercises.map(exercise => (
+                <li key={exercise.exerciseId}>
+                  {exercise.name} 
+                  <small>{exercise.muscleGroup}</small>
+
+                  <ul>
+                    {
+                      exercise.sets.map(set => (
+                        <li key={set.id}>
+                          {set.reps} reps - {set.weight}kg
+                        </li>
+                      ))
+                    }
+                  </ul>
+                </li>
+              ))
+            }
+          </ul>
+
+        </li>
+      ))
+    }
+  </ul>
+
+}
